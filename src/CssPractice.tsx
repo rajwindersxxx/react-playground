1cfaa51f814861cc76cd67bc183feab7002d4013
rajwindersxxx <rajwindersxxx@gmail.com>
2024-12-20 15:21:31 +0530

    fsd

---
+++ src/CssPractice.tsx
@@ -0,0 +1,45 @@
+import styles from './CssPractice.module.css';
+
+export default function CssPractice() {
+  return (
+    <>
+      {/* <div className={styles.container}>
+        <div className={`${styles.item} ${styles['item--1']}`}>1: Orange</div>
+        <div className={`${styles.item} ${styles['item--2']}`}>2: Green</div>
+        <div className={`${styles.item} ${styles['item--3']}`}>3: Violet</div>
+        <div className={`${styles.item} ${styles['item--4']}`}>4: Pink</div>
+        <div className={`${styles.item} ${styles['item--5']}`}>5: Blue</div>
+        <div className={`${styles.item} ${styles['item--6']}`}>6: Brown</div>
+      </div> */}
+      {/* <div className={styles.container2}>
+        <header className={`${styles.item2} ${styles['header']}`}>Header</header>
+        <div className={`${styles.item2} ${styles['small-box-1']}`}>small-box-1</div>
+        <div className={`${styles.item2} ${styles['small-box-2']}`}>small-box-2</div>
+        <div className={`${styles.item2} ${styles['small-box-3']}`}>small-box-3</div>
+        <nav className={`${styles.item2} ${styles['sidebar']}`}>sidebar</nav>
+        <main className={`${styles.item2} ${styles['main-content']}`}>main-content</main>
+        <footer className={`${styles.item2} ${styles['footer']}`}>Footer</footer>
+      </div> */}
+      {/* <div className={styles.container3}>
+        <div className={`${styles.item3} ${styles['item3--1']}`}>Modern</div>
+        <div className={`${styles.item3} ${styles['item3--2']}`}>CSS</div>
+        <div className={`${styles.item3} ${styles['item3--3']}`}>with</div>
+        <div className={`${styles.item3} ${styles['item3--4']}`}>Flexbox</div>
+        <div className={`${styles.item3} ${styles['item3--5']}`}>and</div>
+        <div className={`${styles.item3} ${styles['item3--6']}`}>Grid</div>
+        <div className={`${styles.item3} ${styles['item3--7']}`}>is</div>
+        <div className={`${styles.item3} ${styles['item3--8']}`}>great</div>
+      </div> */}
+      <div className={styles.container4}>
+        <div className={`${styles.item4} ${styles['item4--1']}`}>Modern</div>
+        <div className={`${styles.item4} ${styles['item4--2']}`}>CSS</div>
+        <div className={`${styles.item4} ${styles['item4--3']}`}>with</div>
+        <div className={`${styles.item4} ${styles['item4--4']}`}>Flexbox</div>
+        <div className={`${styles.item4} ${styles['item4--5']}`}>and</div>
+        <div className={`${styles.item4} ${styles['item4--6']}`}>Grid</div>
+        <div className={`${styles.item4} ${styles['item4--7']}`}>is</div>
+        <div className={`${styles.item4} ${styles['item4--8']}`}>great </div>
+      </div>
+    </>
+  );
+}
